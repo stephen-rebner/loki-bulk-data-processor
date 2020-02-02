ccb9cffe74a3989b9b67063de9147f2ec1f50739
stephen.rebner <stephenrebner@gmail.com>
2020-02-02 10:51:08 +0000

    Updating AssemblyInfo

--- LokiBulkDataProcessor/Properties/AssemblyInfo.cs
+++ LokiBulkDataProcessor/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: InternalsVisibleTo("LokiBulkDataProcessor.UnitTests")]
 
 namespace Loki.BulkDataProcessor.Properties
