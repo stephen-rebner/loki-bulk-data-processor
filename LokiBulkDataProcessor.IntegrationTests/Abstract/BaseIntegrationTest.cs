c303bfde98f1393cd8e99a1cad8cbbe3c7cedbdf
Stephen Rebner <stephenrebner@gmail.com>
2023-04-10 16:05:12 +0100

    Refactored BaseIntegrationTest.cs
    Signed-off-by: stephen-rebner <stephenrebner@gmail.com>

--- LokiBulkDataProcessor.IntegrationTests/Abstract/BaseIntegrationTest.cs
+++ LokiBulkDataProcessor.IntegrationTests/Abstract/BaseIntegrationTest.cs
@@ -1,5 +1,4 @@
-﻿
-using Loki.BulkDataProcessor;
+﻿using Loki.BulkDataProcessor;
 using LokiBulkDataProcessor.IntegrationTests.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,9 +7,9 @@ using FluentAssertions;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TestContainers.Container.Abstractions.Hosting;
-//using Container.Database.MsSql.Integration.Tests.Fixtures;
 using TestContainers.Container.Database.Hosting;
 using TestContainers.Container.Database.MsSql;
+
 namespace LokiBulkDataProcessor.IntegrationTests.Abstract
 {
     public abstract class BaseIntegrationTest
@@ -18,37 +17,30 @@ namespace LokiBulkDataProcessor.IntegrationTests.Abstract
         private const string TestDbName = "IntegrationTestsDb";
 
         private MsSqlContainer MsSqlContainer;
-        
+
         protected TestDbContext TestDbContext;
-        
+
         protected IBulkProcessor BulkProcessor;
 
-        protected string GetConnectionString()
-        {
-            return MsSqlContainer.GetConnectionString(TestDbName);
-        }
-        
         [SetUp]
-        protected async Task TestSetup() 
+        protected async Task TestSetup()
         {
             MsSqlContainer = InitContainerTest();
 
             await MsSqlContainer.StartAsync();
 
             var serviceCollection = ConfigureServiceCollection();
-            
-            ConfigureStartUp(serviceCollection);
-            
-            var serviceProvider = serviceCollection.BuildServiceProvider();
 
-            await MsSqlContainer.StartAsync();
+            ConfigureStartUp(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
 
             var dbContextOptionsBuilder = BuildDbContextOptionsBuilder(serviceProvider);
 
             TestDbContext = new TestDbContext(dbContextOptionsBuilder.Options);
 
             CreateDatabase();
-            
+
             BulkProcessor = serviceProvider.GetService<IBulkProcessor>();
         }
 
@@ -71,15 +63,13 @@ namespace LokiBulkDataProcessor.IntegrationTests.Abstract
 
         private MsSqlContainer InitContainerTest()
         {
-            const string username = "sa";
-            const string password = "Pwd12345678!";
-            const string database = "IntegrationTestsDb";
             MsSqlContainer = new ContainerBuilder<MsSqlContainer>()
-                .ConfigureDatabaseConfiguration(username, password, database)
+                .ConfigureDatabaseConfiguration(username: "sa", password: "Pwd12345678!", databaseName: TestDbName)
                 .Build();
 
             return MsSqlContainer;
         }
+
 
         protected async Task<IEnumerable<T>> LoadAllEntities<T>() where T : class
         {
@@ -106,6 +96,11 @@ namespace LokiBulkDataProcessor.IntegrationTests.Abstract
             numberOfRecords.Should().Be(0);
         }
 
+        protected string GetConnectionString()
+        {
+            return MsSqlContainer.GetConnectionString(TestDbName);
+        }
+
         private void CreateDatabase()
         {
             TestDbContext.Database.EnsureDeleted();
@@ -115,19 +110,17 @@ namespace LokiBulkDataProcessor.IntegrationTests.Abstract
         private IServiceCollection ConfigureServiceCollection()
         {
             IServiceCollection services = new ServiceCollection();
-            
+
             services.AddEntityFrameworkSqlServer();
 
             return services;
-            
-            
-            
         }
+
 
         private void ConfigureStartUp(IServiceCollection serviceCollection)
         {
             var startup = new Startup(GetConnectionString());
-            
+
             startup.ConfigureServices(serviceCollection);
         }
     }
